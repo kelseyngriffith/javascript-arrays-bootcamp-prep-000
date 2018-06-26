cc5990c7b2b585003c33b800d564aecc60c210da
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-06-26 19:21:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,10 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [chocolateBars, ...candyString];
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.shift(candyString);
+  return chocolateBars;
+}
