6873bae79f05fc3275da52b64ef8ccc2da6f6408
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-06-26 19:36:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
   return chocolateBars;
 }
 function removeElementFromBeginningOfArray(chocolateBars) {
-  chocolateBars.slice(0);
-  return chocolateBars;
+  return chocolateBars.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   chocolateBars.pop();
