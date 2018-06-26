5f7b3fc6bcef2b37410df6733cbdf90b641d3885
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-06-26 18:49:39 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+function addElementToBeginningOfArray (chocolateBars, 'Snickers') {
+  chocolateBars.unshift('snickers')
+}
+function destructivelyAddElementToBeginningOfArray() {
+
+}
